8dc0e0c0178fb6e91a40b69c4f8abdd140726b10
erhanurgun <erhan.rgn04@gmail.com>
2023-09-24 20:53:18 +0300

    After the follow-up is released, scrolling to the relevant button has been done and incorrect fields have been corrected

--- to-unfollow.js
+++ to-unfollow.js
@@ -1,32 +1,36 @@
 // Let's write the clickUnfollowButtons() function using async/await
 async function clickUnfollowButtons() {
-    // Select all buttons with the specified CSS class
-    const followButtons = document.querySelectorAll(
+    // We select all "Unfollow" buttons
+    const unfollowButtons = document.querySelectorAll(
         'button.artdeco-button.artdeco-button--muted.artdeco-button--2.artdeco-button--secondary.ember-view'
     );
-    for (let i = 0; i < followButtons.length; i++) {
-        const button = followButtons[i];
-        console.log(`Clicked Button #${i + 1}`);
+    for (let i = 0; i < unfollowButtons.length; i++) {
+        const button = unfollowButtons[i];
+        console.log(`Clicking on button #${i + 1}...`);
         button.click();
-        // To set the waiting time, you can make a wait here if necessary
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        // Check for the presence of the second button
+        // After clicking on the button, scroll to where the button is located
+        await new Promise((resolve) => {
+            setTimeout(() => {
+                const buttonPosition = button.getBoundingClientRect();
+                const yOffset = buttonPosition.top - (window.innerHeight / 2);
+                window.scrollBy(0, yOffset);
+                resolve();
+            }, 1000);
+        });
+        // We check the presence of the second button
         const stopFollowingButton = document.querySelector(
             'button.artdeco-modal__confirm-dialog-btn.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view'
         );
         if (stopFollowingButton) {
-            console.log('Found the 2nd button to click, clicking...');
+            console.log('Second button found, clicked...');
             stopFollowingButton.click();
             await new Promise((resolve) => setTimeout(resolve, 1000));
         } else {
-            console.log('2. Buton bulunamadı, işlem devam ediyor');
+            console.log('Second button not found, operation in progress...');
         }
-
-        // Scroll the page a little after each click
-        window.scrollBy(0, 33); // Can shift 33 pixels, adjust as needed
         await new Promise((resolve) => setTimeout(resolve, 200));
     }
-    // After completing the process, you can reload the page
+    // We reload the page after the process is finished
     location.reload();
 }
 
